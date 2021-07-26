1f905903d66d823ae2e009f73332dfeacd8fe34d
Turo-Rudi <jozsa.andrea@gmail.com>
2021-07-26 10:03:10 +0200

    install atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('5202d68fd0a6495a94931e04f9ce5bf4').install();
 
 ReactDOM.render(
   <React.StrictMode>
