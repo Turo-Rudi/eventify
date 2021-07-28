6d141660013375bfb955552878674ec34ec4b197
Turo-Rudi <jozsa.andrea@gmail.com>
2021-07-28 11:21:02 +0200

    add ScatterChart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NumberOfEvents from './NumberOfEvents';
 import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
 import { WarningAlert } from './Alert';
 import WelcomeScreen from './WelcomeScreen';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 import './App.css';
 import './nprogress.css';
@@ -31,21 +32,31 @@ class App extends Component {
         if (this.mounted) {
           this.setState({ events, locations: extractLocations(events) });
         }
-        if (!navigator.onLine) {
-          this.setState({
-            warningText: 'Looks like you are offline. The list of events are being loaded from the cache.'
-          });
-        } else {
-          this.setState({
-            warningText: ''
-          });
-        }
+        // if (!navigator.onLine) {
+        //   this.setState({
+        //     warningText: 'Looks like you are offline. The list of events are being loaded from the cache.'
+        //   });
+        // } else {
+        //   this.setState({
+        //     warningText: ''
+        //   });
+        // }
       });
     }
   }
 
   componentWillUnmount() {
     this.mounted = false;
+  }
+
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return { city, number };
+    })
+    return data;
   }
 
   updateEvents = (location, eventsToShow) => {
@@ -69,14 +80,29 @@ class App extends Component {
   }
 
   render() {
-    if (this.state.showWelcomeScreen === undefined)
-      return <div className="App" />
+    // if (this.state.showWelcomeScreen === undefined)
+    //   return <div className="App" />
+    const { locations, events, numberOfEvents } = this.state;
     return (
       <div className="App">
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} NumberOfEvents={this.state.NumberOfEvents} />
-        <NumberOfEvents updateNumberOfEvents={(e) => this.updateNumberOfEvents(e)} />
+        <h1>Eventify your life!</h1>
+        <CitySearch locations={locations} updateEvents={this.updateEvents} />
+        <NumberOfEvents updateNumberOfEvents={(e) => this.updateNumberOfEvents(e)} numberOfEvents={numberOfEvents} />
         <WarningAlert text={this.state.warningText} />
-        <EventList events={this.state.events} />
+
+        <h4>Events in each city</h4>
+
+        <ResponsiveContainer height={400}>
+          <ScatterChart margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+
+        <EventList events={events} />
         <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
       </div>
     );
